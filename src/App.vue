94954d419ee94ec5ef39490c1532983e23d8f6c1
itgo <itgo@weboutin.com>
2023-02-22 23:47:54 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,69 @@
 <template>
   <div id="app">
-    <div v-if="wsConnecting">
-      正在建立连接 ..
+    <div v-if="wsConnecting" class="loading">
+      <svg width='40px' height='40px' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid" class="uil-default">
+        <rect x="0" y="0" width="100" height="100" fill="none" class="bk"></rect>
+        <!--以一个点为原点，按一定角度旋转绘制矩形，并在矩形的opacity属性上添加上渐变效果 -->
+        <!--rect绘制x y 是矩形左上角的坐标，width height是矩形的宽高，rx ry为矩形的圆角radius  rotate 旋转  -->
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2'
+          transform='rotate(0 50 50) translate(0 -30)'>
+          <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0s' repeatCount='indefinite' />
+        </rect>
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2'
+          transform='rotate(30 50 50) translate(0 -30)'>
+          <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.08333333333333333s'
+            repeatCount='indefinite' />
+        </rect>
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2'
+          transform='rotate(60 50 50) translate(0 -30)'>
+          <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.16666666666666666s'
+            repeatCount='indefinite' />
+        </rect>
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2'
+          transform='rotate(90 50 50) translate(0 -30)'>
+          <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.25s' repeatCount='indefinite' />
+        </rect>
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2'
+          transform='rotate(120 50 50) translate(0 -30)'>
+          <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.3333333333333333s'
+            repeatCount='indefinite' />
+        </rect>
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2'
+          transform='rotate(150 50 50) translate(0 -30)'>
+          <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.4166666666666667s'
+            repeatCount='indefinite' />
+        </rect>
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2'
+          transform='rotate(180 50 50) translate(0 -30)'>
+          <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.5s' repeatCount='indefinite' />
+        </rect>
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2'
+          transform='rotate(210 50 50) translate(0 -30)'>
+          <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.5833333333333334s'
+            repeatCount='indefinite' />
+        </rect>
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2'
+          transform='rotate(240 50 50) translate(0 -30)'>
+          <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.6666666666666666s'
+            repeatCount='indefinite' />
+        </rect>
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2'
+          transform='rotate(270 50 50) translate(0 -30)'>
+          <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.75s' repeatCount='indefinite' />
+        </rect>
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2'
+          transform='rotate(300 50 50) translate(0 -30)'>
+          <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.8333333333333334s'
+            repeatCount='indefinite' />
+        </rect>
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2'
+          transform='rotate(330 50 50) translate(0 -30)'>
+          <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.9166666666666666s'
+            repeatCount='indefinite' />
+        </rect>
+      </svg>
+      <div class="loading-text">正在建立连接 ...</div>
     </div>
     <div v-else>
       <MainGame v-if="!inMenu" />
@@ -69,3 +131,17 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.loading {
+  position: fixed;
+  top: 40%;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+}
+
+.loading-text {
+  margin-top: 10px;
+}
+</style>
